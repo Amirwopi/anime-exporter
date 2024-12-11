769aada7a4bdf7c52f0656ab6fb0eea138d2c84b
Amirwopi <56787008+Amirwopi@users.noreply.github.com>
2024-12-11 14:13:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -30,18 +30,18 @@ app.get('/', async (req, res) => {
     // دریافت `slug` و `quality` از query parameters
     const { anime_id, quality } = req.query;
 
-    if (!episodeid || !quality) {
+    if (!anime_id || !quality) {
         return res.status(400).json({ error: 'Both slug and quality parameters are required' });
     }
 
     try {
-        const apiResponse = await axios.get(`https://api-wopi.amirwopi.workers.dev/anime/${episodeid}`);
+        const apiResponse = await axios.get(`https://api-wopi.amirwopi.workers.dev/anime/${anime_id}`);
         const data = apiResponse.data;
 
         const selectedLink = data.DownloadLinks.find(link => link.Quality.toLowerCase() === quality.toLowerCase());
 
         if (!selectedLink) {
-            return res.status(404).json({ error: `Quality ${quality} not found for ${episodeid}` });
+            return res.status(404).json({ error: `Quality ${quality} not found for ${anime_id}` });
         }
 
         const extractedLinks = await extractLinksFromDirectory(selectedLink.URL);
@@ -49,7 +49,7 @@ app.get('/', async (req, res) => {
         const episodeLinks = {};
         extractedLinks.forEach((link, index) => {
             const episodeNumber = index + 1;
-            episodeLinks[`${episodeid}-episode-${episodeNumber}`] = link;
+            episodeLinks[`${anime_id}-episode-${episodeNumber}`] = link;
         });
 
         res.json({
